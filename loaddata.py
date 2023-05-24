4bbd2946fd91d018a84a5bd2188366db47cb090d
Suboms <40143366+Suboms@users.noreply.github.com>
2023-05-24 22:29:21 +0100

    m

--- loaddata.py
+++ loaddata.py
@@ -1,24 +1,24 @@
-import json
-from django.core.management.base import BaseCommand
-from schools.models import Country, State
-import os
+# import json
+# from django.core.management.base import BaseCommand
+# from schools.models import Country, State
+# import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digirepo.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digirepo.settings")
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        with open("/home/dubsy/virtualenvs/djangoproject/digirepo/countries.json") as f:
-            data = json.load(f)
-            for item in data:
-                country = Country.objects.create(
-                    name=item["name"],
-                    country_code=item["code3"],
-                    capital=item["capital"],
-                )
-                for state_data in item["states"]:
-                    state = State(
-                        country=country,
-                        name=state_data["name"],
-                    )
-                    state.save()
+# class Command(BaseCommand):
+#     def handle(self, *args, **options):
+#         with open("/home/dubsy/virtualenvs/djangoproject/digirepo/countries.json") as f:
+#             data = json.load(f)
+#             for item in data:
+#                 country = Country.objects.create(
+#                     name=item["name"],
+#                     country_code=item["code3"],
+#                     capital=item["capital"],
+#                 )
+#                 for state_data in item["states"]:
+#                     state = State(
+#                         country=country,
+#                         name=state_data["name"],
+#                     )
+#                     state.save()
